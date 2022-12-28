8c34d9a83695819d339eaf272443ca2267cc1432
DoTheBestMayB <send1684@gmail.com>
2022-12-29 01:22:00 +0900

    refactor: MainActivity MVC Contract 코드 삭제

--- app/src/main/java/com/github/dodobest/attendhelper/MainActivity.kt
+++ app/src/main/java/com/github/dodobest/attendhelper/MainActivity.kt
@@ -5,14 +5,28 @@ import android.os.Bundle
 import android.content.Intent
 import com.github.dodobest.attendhelper.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity(), MainContract.View {
-    private lateinit var binding: ActivityMainBinding
+class MainActivity : AppCompatActivity() {
+
+    private var _binding: ActivityMainBinding? = null
+    private val binding: ActivityMainBinding
+        get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        _binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        setListener()
+    }
+
+    override fun onDestroy() {
+        _binding = null
+
+        super.onDestroy()
+    }
+
+    private fun setListener() {
         binding.webViewButton.setOnClickListener {
             val intent = Intent(this, WebViewActivity::class.java)
             startActivity(intent)
